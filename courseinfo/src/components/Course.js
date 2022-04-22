552e652f7e7d27c0a00e5433e573b0395ba06bc3
Omar Baltaji <omar.baltaji@app4legal.com>
2022-04-22 12:26:43 +0300

    Course information step 6 and 7

---
+++ courseinfo/src/components/Course.js
@@ -0,0 +1,19 @@
+import Header from './Header';
+import Content from './Content';
+import Total from './Total';
+
+const Course = ({ course }) => {
+    const sum = course.parts.reduce((sum, part) => {
+        return sum += part.exercises
+    }, 0);
+    
+    return (
+        <div>
+        <Header course={course.name} />
+        <Content parts={course.parts} />
+        <Total sum={sum} />
+        </div>
+    );
+}
+
+export default Course;
