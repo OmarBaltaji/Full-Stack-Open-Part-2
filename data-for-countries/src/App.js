06d94491fb5e13574093a0c2478a989fd76a402f
Omar Baltaji <omar.baltaji@app4legal.com>
2022-04-23 22:11:05 +0300

    Data for countries step1

--- data-for-countries/src/App.js
+++ data-for-countries/src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [filteredCountries, setFilteredCountries] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://restcountries.com/v3.1/all')
+    .then(res => {
+      console.log(res.data);
+      setCountries(res.data);
+    });
+  }, []);
+
+  const onChangeHandler = (event) => {
+    const typedCountryLowerCase = event.target.value.trim().toLowerCase().replace(/\w\S*/g, (w) => (w.replace(/^\w/, (c) => c.toUpperCase())));;
+    const countriesToFilter = countries.filter(country => country.name.common.includes(typedCountryLowerCase));
+    setFilteredCountries(countriesToFilter);
+
+    if(!typedCountryLowerCase) {
+      setFilteredCountries([]);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <span>Find countries</span> &nbsp;
+        <input onChange={onChangeHandler} />
+      </div>
+      <div>
+      {
+        filteredCountries.length === 0 ?
+        <span>No countries found</span>
+        :
+        (filteredCountries.length === 1 ?
+          filteredCountries.map(country => 
+            <div key={country.cca2}>
+              <h2>{country.name.common}</h2>
+              <div>capital {country.capital}</div>
+              <div>area {country.area}</div>
+              <h4>languages:</h4>
+              <ul>
+                {Object.entries(country.languages).map((lang) => <li key={lang[0]}>{lang[1]}</li>)}
+              </ul>
+              <div>
+                <img src={country.flags.png} alt="country's flag" />
+              </div>
+            </div>
+          )
+          :
+          (filteredCountries.length > 10 
+            ? 
+            <span>Too many matches, specify another filter</span>
+            :
+            filteredCountries.map(country => <div key={country.cca2}>{country.name.common}</div>)
+          )
+        )
+      }
+      </div>
     </div>
   );
 }
